5da7202eac3a32cce7ab1ba141d9f876f717f632
whies <1543162457@qq.com>
2026-07-26 21:20:54 +0800

    自动微分模块

--- 02_tensor_operation/08_tensor_ad.py
+++ 02_tensor_operation/08_tensor_ad.py
@@ -1,5 +1,4 @@
-#自动微分的流程演示
-#其中损失函数仅仅举例用以演示过程
+#自动微分的流程
 
 #权重更新 ： w新 = w旧 - 学习率 * 梯度
 #梯度计算：梯度 = 损失函数的导数
@@ -12,6 +11,8 @@
 #可以通过detach().numpy()拷贝一个不进行自动微分的ndarray
 #这个和原来的张量是共享内存的
 
+#dm01用来演示一次反向传播更新权重的过程,dm02用来演示多次反向传播更新权重的过程
+#dm03用来演示detach()函数的使用,dm04用来演示一个自动微分的实际应用过程
 
 import torch
 #dm01用来演示一次反向传播更新权重的过程
@@ -60,10 +61,49 @@ def dm03():
     print(f't1:{t1},type:{type(t1)}')
     print(f'n2:{n2},type:{type(n2)}')
 
+#dm04用来演示一个自动微分的实际应用过程
+def dm04():
+    #定义x，假设是2行5列的全一张量(表示：特征)
+    #定义y，假设是3行6列的全零张量(表示:标签(真实值))
+    x = torch.ones(2,5)
+    y = torch.zeros(2,6)
+    print(f'x:{x}')
+    print(f'y:{y}')
+
+
+    #初始化可自动微分的权重和偏置
+    #z是预测值   z = x @ w + b
+    #因此权重w是五行六列，偏置b是6个
+    #因为x是2行5列，y是2行6列，x @ w = 2行6列，所以w应该是5行6列
+    #b与输出特征数绑定
+    w = torch.randn(5,6,requires_grad = True)
+    b = torch.randn(6,requires_grad = True)
+    print(f'w:{w}')
+    print(f'b:{b}')
+    #前向传播
+    z = x @ w + b
+    print(f'z:{z}')
+
+    #损失函数
+    criterion = torch.nn.MSELoss()
+    loss = criterion(z,y)
+
+    #反向传播
+    loss.sum().backward()
+
+    #后续就是w = w - 学习率 * 梯度    b = b - 学习率 * 梯度
+    #更新权重和偏置
+    w.data = w.data - 0.01 * w.grad
+    b.data = b.data - 0.01 * b.grad
+    print(f'w:{w},b:{b}')
+
+
+
 
 #测试函数
 if __name__ == '__main__':
     dm01()
     dm02()
     dm03()
+    dm04()
 
